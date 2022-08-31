5f4a77eae2bb64a82757f1f600f5a956c00f14a6
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-08-31 10:41:13 +0400

    3 new files were added

--- conftest.py
+++ conftest.py
@@ -5,9 +5,10 @@ from selene import be
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.config.window_width = 100
-    browser.config.window_height = 100
+    browser.config.window_width = 1000
+    browser.config.window_height = 1000
     browser.open('https://google.com')
+
 
 @pytest.fixture()
 def search_selene():
--- test_search_in_browser.py
+++ test_search_in_browser.py
@@ -3,10 +3,15 @@ from selene import have
 
 
 def test_search_selene_positive(search_selene):
-    assert browser.element('[id="search"]').should(have.text('yashaka/selene:'))
+    browser.element('[id="search"]').should(have.text('yashaka/selene:'))
+    # Использование assert и should(have) избыточно, достаточно применить что-то одно
+    # .should - имеет встроенные ожидания
+    # а все что мы тестируем в вебе - динамическое, и ожидание нам нужно всегда
+    # Поэтому для всех асертов по веб элементам - используем .should
+
     print("Positive")
 
 
 def test_search_selene_negative(search_selene):
-    assert browser.element('[id="search"]').should(have.no.text('harry potter'))
+    browser.element('[id="search"]').should(have.no.text('harry potter'))
     print("Negative")
